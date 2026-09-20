eca47b1baf157426abe622cddfab6b7a6c485489
Hexye <dragonsale22@gmail.com>
2026-09-21 00:46:24 +0200

    fixing note md

--- src/JevPR/services/pull_request.py
+++ src/JevPR/services/pull_request.py
@@ -14,6 +14,7 @@ from JevPR.github.client import GitHubClient
 from JevPR.providers.jev import JevProvider
 from JevPR.services.evaluation import EvaluationOutcome, EvaluationService
 
+
 logger = logging.getLogger(__name__)
 
 REVIEW_WORTHY_PR_ACTIONS = {"opened", "reopened", "synchronize", "ready_for_review"}
@@ -161,6 +162,7 @@ async def _apply_route_to_github(
     if has_default_policy:
         route_summary = f"""{route_summary}
         
+
         *Note: This routing was determined by the default policy.*
         The repository does not have a custom routing configuration.
 
