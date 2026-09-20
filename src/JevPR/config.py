b4d8c8e8cfe6cfefd691921f14ce0aad50538168
Hexye <dragonsale22@gmail.com>
2026-09-21 00:09:36 +0200

    test note

--- src/JevPR/config.py
+++ src/JevPR/config.py
@@ -9,7 +9,6 @@ import yaml
 from pydantic import BaseModel, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class ReviewerConfig(BaseModel):
     username: str
     github_id: int | None = None
